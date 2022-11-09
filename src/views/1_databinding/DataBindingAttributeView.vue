39fe4de20eaa5b574675d062733acce932bc3158
blue4313 <blue4313@gmail.com>
2022-11-10 00:19:52 +0900

    html, checkbox, radio, attr 바인딩

---
+++ src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" name="" id="" v-bind:value="userId" readonly />
+    <br />
+    <input type="text" name="" id="" :value="userName" readonly />
+    <br />
+    <img :src="imgSrc" alt="" style="width: 100px; height: auto" />
+    <br />
+    <input type="search" name="" id="" v-model="txt1" />
+    <button :disabled="txt1 === ''">조회</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'kijun.lee.2022',
+      userName: '이기준',
+      imgSrc: 'https://cdn.mhns.co.kr/news/photo/202203/523953_634618_3856.jpg',
+      txt1: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
--- src/views/1_databinding/DataBindingHtmlView.vue
+++ src/views/1_databinding/DataBindingHtmlView.vue
@@ -1,14 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <div>{{ htmlString }}</div>
+    <div v-html="htmlString"></div>
+  </div>
 </template>
 <script>
 export default {
   components: {},
   data() {
     return {
-      sampleData: ''
+      htmlString: '<p style="color:red;">빨간색 문자</p>'
     }
   },
+  setup() {},
   created() {},
   mounted() {},
   unmounted() {},
